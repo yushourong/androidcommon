adf62004afb1ac2503cb7c4e977b0a19b29292b9
knjin <289601142@qq.com>
2016-11-22 14:53:32 +0800

    websocket使用
    websocket使用

--- app/src/main/java/cn/cloudwalk/common/MainActivity.java
+++ app/src/main/java/cn/cloudwalk/common/MainActivity.java
@@ -3,11 +3,23 @@ package cn.cloudwalk.common;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import cn.cloudwalk.common.websocket.websocketUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //websocket使用
+        final websocketUtil ws = new websocketUtil();
+        ws.connectStranger("","");//输入服务器ip与摄像头编号
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ws.client.connect();
+            }
+        }).start();
     }
 }
